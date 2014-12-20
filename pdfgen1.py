8809571008d33c047a443431800353832f32db66
sopier <sopier@gmail.com>
2014-12-20 07:18:45 +0700

    testing generate html file

---
+++ pdfgen1.py
@@ -0,0 +1,28 @@
+import os
+from jinja2 import Environment, FileSystemLoader
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE_ENVIRONMENT = Environment(
+    autoescape=False,
+    loader=FileSystemLoader(os.path.join(PATH, 'templates')),
+    trim_blocks=False)
+
+
+def render_template(template_filename, context):
+    return TEMPLATE_ENVIRONMENT.get_template(template_filename).render(context)
+
+
+def create_index_html():
+    fname = "output.html"
+    urls = ["example.com", "example.net", "example.co.id"]
+    context = {
+        "urls": urls
+        }
+
+    with open(fname, "w") as f:
+        html = render_template("index.html", context)
+        f.write(html)
+
+
+if __name__ == "__main__":
+    create_index_html()
