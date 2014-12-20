6806f193efac1db85e8150a0a5ddd2f24a91a96e
sopier <sopier@gmail.com>
2014-12-20 09:53:03 +0700

    generating our first .tex file

---
+++ pdfgen.py
@@ -0,0 +1,42 @@
+import os
+from jinja2 import Environment, FileSystemLoader
+
+
+"""
+source: http://pythonadventures.wordpress.com/2014/02/25/jinja2-example-for-generating-a-local-file-using-a-template/
+
+1. create .tex template
+2. put some variables in it
+3. generate each .tex file
+4. convert it to pdf with pdftolatex
+5. done!
+"""
+
+
+PATH = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE_ENVIRONMENT = Environment(
+    autoescape=False,
+    loader=FileSystemLoader(os.path.join(PATH, 'templates')),
+    trim_blocks=False)
+
+
+def render_template(template_filename, context):
+    return TEMPLATE_ENVIRONMENT.get_template(template_filename).render(context)
+
+
+def create_tex():
+    # output/generated file
+    fname = "output.tex"
+    # variables we will use in template
+    urls = ["example.com", "example.net", "example.co.id"]
+    context = {
+        "urls": urls
+        }
+
+    with open(fname, "w") as f:
+        html = render_template("index.tex", context)
+        f.write(html)
+
+
+if __name__ == "__main__":
+    create_tex()
