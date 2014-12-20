021d97e261992a22c2a8380249d0665f7edbf066
sopier <sopier@gmail.com>
2014-12-21 06:19:30 +0700

    alter landinger

--- pdfgen.py
+++ pdfgen.py
@@ -1,6 +1,9 @@
 import os
 from jinja2 import Environment, FileSystemLoader
 import hashlib
+import subprocess
+from unidecode import unidecode
+
 
 """
 source: http://pythonadventures.wordpress.com/2014/02/25/jinja2-example-for-generating-a-local-file-using-a-template/
@@ -13,6 +16,7 @@ source: http://pythonadventures.wordpress.com/2014/02/25/jinja2-example-for-gene
 """
 
 
+# load the templates dir
 PATH = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_ENVIRONMENT = Environment(
     autoescape=False,
@@ -20,15 +24,16 @@ TEMPLATE_ENVIRONMENT = Environment(
     trim_blocks=False)
 
 
+# ready to render
 def render_template(template_filename, context):
     return TEMPLATE_ENVIRONMENT.get_template(template_filename).render(context)
 
 
-def create_tex():
+def create_tex(title):
     # output/generated file
     fname = "output.tex"
     # variables we will use in template
-    title = "2001 ford taurus automatic transmission wiring schematic"
+    # title = "2001 ford taurus automatic transmission wiring schematic"
     # generate unique id for each book
     uid = hashlib.md5(title).hexdigest()
     # context is the container of our data
@@ -40,9 +45,27 @@ def create_tex():
         f.write(html)
 
 
+# build clean slug for filename
+import re
+_punct_re = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
+
+def slugify(text, delim=u'-'):
+    """Generates an ASCII-only slug."""
+    result = []
+    for word in _punct_re.split(text.lower()):
+        result.extend(unidecode(word).split())
+    return unicode(delim.join(result))
+
+
 if __name__ == "__main__":
+    # build list of titles
+    titles = ["2001 ford taurus automatic transmission wiring schematic"]
     # loop through list of bunch setem
-    # generate the tex file
-    # generate the pdf file
-    # move the pdf into separate folder
-    create_tex()
+    for title in titles:
+        # generate the tex file
+        create_tex(title)
+        # generate the pdf file
+        subprocess.call(["pdflatex", "output.tex"])
+        # move the pdf into separate folder
+        subprocess.call(["mv", "output.pdf", slugify(unicode(title)) + ".pdf"])
+
